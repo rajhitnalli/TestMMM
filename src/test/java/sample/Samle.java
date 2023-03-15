c9fc07fdeb1ae39fcfb5106789420d590b778899
rajhitnalli <126956707+rajhitnalli@users.noreply.github.com>
2023-03-15 15:34:41 +0530

    Update Samle.java

--- src/test/java/sample/Samle.java
+++ src/test/java/sample/Samle.java
@@ -7,6 +7,7 @@ public class Samle {
 		System.out.println("mfcc");
 		//pull from git to eclips
 		//push from eclips to github
-		//push fro eclips
+		//push fro eclips commands
+		//pull from git command
 	}
 }
